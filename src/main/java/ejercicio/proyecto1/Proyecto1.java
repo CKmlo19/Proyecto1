87fdc71c6c71d5e38ea830908a48cbee0fffbe86
Camilo Orocú <luiscamilooc@gmail.com>
2023-10-01 17:32:55 -0600

    Carga inicial

---
+++ src/main/java/ejercicio/proyecto1/Proyecto1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ejercicio.proyecto1;
+
+/**
+ *
+ * @author luisc
+ */
+public class Proyecto1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
