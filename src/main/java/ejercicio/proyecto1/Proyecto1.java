4fdc63b9d9e4b615d89c8f797102f39a542bacfe
Camilo Orocú <luiscamilooc@gmail.com>
2023-10-04 23:18:43 -0600

    Creando algunos atributos para el Personaje

--- src/main/java/ejercicio/proyecto1/Proyecto1.java
+++ src/main/java/ejercicio/proyecto1/Proyecto1.java
@@ -18,6 +18,7 @@ public class Proyecto1 {
         Ventana v1 = new Ventana();
         v1.setVisible(true);
         v1.getContentPane().setBackground(Color.white);
+        Personaje p1 = new Personaje("Zombie", 100, 2, 3, 1, 0, "de contacto", 100);
         
     }
 }
---
+++ src/main/java/ejercicio/proyecto1/Zombie.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio.proyecto1;
+
+/**
+ *
+ * @author luisc
+ */
+
+// clase que hereda de personaje
+public class Zombie extends Personaje{
+    public Zombie(String Nombre, int vida, int cantidad_golpes, int nivel, int campos, int nivel_aparicion, String tipo, int rango) {
+        super(Nombre, vida, cantidad_golpes, nivel, campos, nivel_aparicion, tipo, rango);
+    }
+}
